0d609fdb3edbd8ceeac32f35f486aae17c871411
JeroT1 <JeroT1YT@yandex.ru>
2025-12-24 01:13:28 +0300

    7 задание

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
+<form action="index.php" method="post">
+    <label>Username:</label><br>
+    <input type="text" name="username"><br>
+    <label>Password:</label><br>
+    <input type="password" name="password"><br>
+    <input type="submit" value="Login">
+</form>
+
+
+
 <?php
-$x = 10;
-$y = 2;
-$z = $x + $y;
-echo $z; // 12
-$counter = 0;
-$counter++;
-echo $counter; // 1
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    echo "Username: {$username}<br>";
+}
 ?>
