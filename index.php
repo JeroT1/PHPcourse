a3d00c664c3c296d2809691206d3a2daae694cf2
JeroT1 <JeroT1YT@yandex.ru>
2025-12-24 01:07:16 +0300

    4 задание

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+echo "I love pizza<br>";
+// однострочный комментарий
+/*
+многострочный комментарий
+*/
+?>
+<!DOCTYPE html>
+<html>
+<body>
+    <button>Order a pizza</button>
+</body>
+</html>
